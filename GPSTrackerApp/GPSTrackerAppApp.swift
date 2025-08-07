fc7e71acbf82784ef9d1bbe44d1b36b6fcc20fc4
Shuhan Yi <enian991122@gmail.com>
2025-08-07 10:56:26 +0800

    Initial Commit

---
+++ GPSTrackerApp/GPSTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPSTrackerAppApp.swift
+//  GPSTrackerApp
+//
+//  Created by Shuhan Yi on 2025/8/7.
+//
+
+import SwiftUI
+
+@main
+struct GPSTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GPSTrackerAppTests/GPSTrackerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  GPSTrackerAppTests.swift
+//  GPSTrackerAppTests
+//
+//  Created by Shuhan Yi on 2025/8/7.
+//
+
+import Testing
+@testable import GPSTrackerApp
+
+struct GPSTrackerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
